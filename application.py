711ae229f805dfb256e2c23f3b56ca960bb8e48d
Ankit193200 <ankit1932004@gmail.com>
2023-10-01 11:32:45 +0530

    ml predict

--- application.py
+++ application.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import StandardScaler
 application = Flask(__name__)
 app=application
 
+
 ## import ridge regresor model and standard scaler pickle
 ridge_model=pickle.load(open('models/ridge.pkl','rb'))
 standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
